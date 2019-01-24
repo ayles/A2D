805bfd74670329ff168688af74cfff56e3c751cc
Alexander <selya.main@gmail.com>
2019-01-25 01:04:24 +0300

    Linux build fix try.

--- src/cmake/build_shaders.cmake
+++ src/cmake/build_shaders.cmake
@@ -14,16 +14,14 @@ file(MAKE_DIRECTORY ${SHADERS_GENERATION_PATH})
 subdirlist(SHADERS_DIRS ${SHADERS_ROOT_DIR})
 
 foreach(SHADER_DIR ${SHADERS_DIRS})
+    message(STATUS "Shader \"${SHADER_DIR}\":")
     execute_process(
             COMMAND ${GLSLCC_EXECUTABLE}
             --vert=${SHADERS_ROOT_DIR}/${SHADER_DIR}/vertex.glsl
             --frag=${SHADERS_ROOT_DIR}/${SHADER_DIR}/fragment.glsl
             --output=${SHADERS_GENERATION_PATH}/${SHADER_DIR}.glsl
             --lang=${SHADERS_TARGET_LANG}
-            --profile=${SHADERS_TARGET_PROFILE}
-            OUTPUT_VARIABLE out ERROR_VARIABLE err)
-
-    message(STATUS "Shader \"${SHADER_DIR}\":\n${out} ${err}")
+            --profile=${SHADERS_TARGET_PROFILE})
 
     file(READ "${SHADERS_GENERATION_PATH}/${SHADER_DIR}_vs.glsl" VERTEX_SHADER)
     file(READ "${SHADERS_GENERATION_PATH}/${SHADER_DIR}_fs.glsl" FRAGMENT_SHADER)
